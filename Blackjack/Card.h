f791028902f54fd9952d36e54699a6233d300673
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-03-20 22:43:37 +0300

    worked on Card.h and Deck.h--incomplete

--- Blackjack/Card.h
+++ Blackjack/Card.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
+enum Suit {
+	spade, club, diamond, heart
+};
+
+class Card {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private:
+	Suit suit;
+	int number;
+
+};
